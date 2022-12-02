e2049c699e81bff40105cdf5545d5e7f5cc26712
jordimasip <jordimasipb@gmail.com>
2022-12-02 21:44:09 +0100

    day 1

---
+++ src/bin/day1/day1.rs
@@ -0,0 +1,17 @@
+fn main() {
+
+    let data = include_str!("input.txt");
+    let info = data.split("\n\n");
+    let mut parsed_data: Vec<u32> = info
+        .map(|line| {
+            line.split("\n")
+                .flat_map(|n| n.parse::<u32>())
+                .sum::<u32>()
+        }).collect();
+
+    parsed_data.sort();
+    parsed_data.reverse();
+
+    println!("{}", parsed_data[0]);
+    println!("{:?}", parsed_data[0] + parsed_data[1] + parsed_data[2]);
+}
