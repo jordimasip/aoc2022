553ba3a544e9827ef2cb5999f05d07d04a3a6592
jordimasip <jordimasipb@gmail.com>
2022-12-10 10:36:13 +0100

    day 7 with ThePrimeagen

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,8 +1,47 @@
+use std::fs;
 
-fn main() {
+use anyhow::Result;
 
-    let data = include_str!("./sample7.txt");
 
-    let commands: Vec<&str> = data.split("\n").collect();
-    println!("{:?}", commands);
+fn main() -> Result<()>{
+
+    let input = fs::read_to_string("src/bin/input7.txt")?;
+    let mut stack = vec![
+        ("/", 0)
+    ];
+
+    let report_amount = 100_000;
+    let mut total = 0;
+
+    for line in input.lines().filter(|x| !x.is_empty()){
+       if line == "$ cd /" || line == "$ ls" {
+           continue;
+       }
+
+       if line.starts_with("$ cd ") {
+           let dir = &line[5..];
+           if dir == ".." {
+               let (_, amount) = stack.pop().unwrap();
+               if amount <= report_amount {
+                   total += amount;
+               }
+               stack.last_mut().unwrap().1 += amount;
+           } else {
+               stack.push((dir, 0));
+           }
+           continue;
+       }
+
+       let (amount, _) = line.split_once(" ").unwrap();
+
+       if let Ok(amount) = amount.parse::<usize>() {
+           stack.last_mut().unwrap().1 += amount;
+       } else if amount == "dir" {
+          // ignore 
+       }
+
+    }
+    println!("{}", total);
+
+    return Ok(());
 }
